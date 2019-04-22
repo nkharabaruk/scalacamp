f1c3d2b8ac79322d575a52f0e0e17989ebb97bd8
Nataliia Kharabaruk <nkharabaruk@sciforce.solutions>
2019-04-22 21:22:41 +0300

    rewrite retry to future

--- src/main/scala/example/Retrier.scala
+++ src/main/scala/example/Retrier.scala
@@ -1,16 +1,20 @@
 package example
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future}
 import scala.annotation.tailrec
 import scala.concurrent.duration._
 
 class Retrier {
 
   @tailrec
-  final def retry[A](block: () => A,
+  final def retry[A](block: () => Future[A],
                      acceptResult: A => Boolean,
-                     retries: List[FiniteDuration]): A = {
-    val result = block.apply()
-    if (acceptResult.apply(result) || retries.isEmpty)
+                     retries: List[FiniteDuration]): Future[A] = {
+    val result = block()
+    var accept = false
+    for { a <- result } yield accept = acceptResult.apply(a)
+    if (accept || retries.isEmpty)
       result
     else {
       Thread.sleep(retries.head.toMillis)
--- src/test/scala/example/RetrierTest.scala
+++ src/test/scala/example/RetrierTest.scala
@@ -1,6 +1,8 @@
 package example
 
 import org.scalatest.{FlatSpec, Matchers}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 
 class RetrierTest extends FlatSpec with Matchers {
@@ -8,38 +10,42 @@ class RetrierTest extends FlatSpec with Matchers {
   val retrier = new Retrier()
 
   "The retry method call" should "return valid result" in {
-    retrier.retry[Int](
-      block = () => 1 + 1,
+    val result = Await.result(retrier.retry[Int](
+      block = () => Future { 1 + 1 },
       acceptResult = res => res % 2 == 0,
       retries = List[FiniteDuration](0.seconds, 1.seconds, 2.seconds)
-    ) shouldEqual 2
+    ), 500 millis)
+    result shouldEqual 2
   }
 
   "The retry method call with empty retries" should "return first result" in {
-    retrier.retry[Int](
-      block = () => 1 + 2,
+    val result = Await.result(retrier.retry[Int](
+      block = () => Future { 1 + 2 },
       acceptResult = res => res % 2 == 0,
       retries = List.empty
-    ) shouldEqual 3
+    ), 500 millis)
+    result shouldEqual 3
   }
 
   "The retry method call" should "call 4 times and return last invalid result" in {
     var counter = 0
-    retrier.retry[Int](
-      block = () => { counter = counter + 1; counter },
+    val result = Await.result(retrier.retry[Int](
+      block = () => Future { counter = counter + 1; counter },
       acceptResult = res => res > 10,
       retries = List[FiniteDuration](0.seconds, 1.seconds, 2.seconds)
-    ) shouldEqual 4
+    ), 500 millis)
+    result shouldEqual 4
     counter shouldEqual 4
   }
 
   "The retry method call" should "wait 3 seconds and return last invalid result" in {
     val start = System.currentTimeMillis()
-    retrier.retry[Int](
-      block = () => 1 + 1,
+    val result = Await.result(retrier.retry[Int](
+      block = () => Future { 1 + 1 },
       acceptResult = res => false,
       retries = List[FiniteDuration](0.seconds, 1.seconds, 2.seconds)
-    ) shouldEqual 2
+    ), 500 millis)
+    result shouldEqual 2
     val end = System.currentTimeMillis()
     val duration = ( end - start ) / 1000
     duration shouldEqual 3
